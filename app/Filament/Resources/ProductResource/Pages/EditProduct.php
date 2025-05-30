f7d7ad32554d9f9ef3820adbd2a9acb5e4cd3ee5
Idam Achmad Faizin <idamachmadfaizin@gmail.com>
2025-05-30 23:55:31 +0700

    feat: delete old uploaded image in product.

--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Storage;
 
 class EditProduct extends EditRecord
 {
@@ -18,5 +19,15 @@ class EditProduct extends EditRecord
             Actions\ForceDeleteAction::make(),
             Actions\RestoreAction::make(),
         ];
+    }
+
+//Storage::disk('public')->delete($file)
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if ($this->record->isDirty("images") || $this->record->images === null) return $data;
+        Storage::disk('public')->delete($this->record->images);
+
+        return $data;
     }
 }
